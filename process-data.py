d34726d455d219cdec191a10e0d85a4845419093
Gabe Joseph <gjoseph92@gmail.com>
2014-12-13 17:18:31 -0500

    Pickling, canning, making of preserves, etc

--- process-data.py
+++ process-data.py
@@ -1,7 +1,13 @@
 from __future__ import print_function
 import sys
+import cPickle
+import os.path
+
 import ijson
 from pygeocoder import Geocoder
+
+def print_err(msg):
+    print(msg, file=sys.stderr)
 
 def softness(ascents, grade):
     soft = 0
@@ -9,14 +15,14 @@ def softness(ascents, grade):
     hard = 0
     for ascent in ascents:
         if not type(ascent) is dict:
-            print( "ERROR: ascent is a {}: {}".format(type(ascent), ascent), file=sys.stderr )
+            print_err( "ERROR: ascent is a {}: {}".format(type(ascent), ascent) )
             continue
 
         asc_grade = ascent['grade']
         asc_comment = ascent['comment']
 
         if not asc_grade[0].isdigit():
-            print( "Not a real grade: {}".format(asc_grade), file=sys.stderr )
+            print_err( "Not a real grade: {}".format(asc_grade) )
             continue
 
         if asc_grade == grade:
@@ -30,12 +36,12 @@ def softness(ascents, grade):
             if not asc_comment.startswith("Hard"):  # grades are lexicographically comparable, nice
                 soft += 1
             else:
-                print( ascent["climber"] + " is an idiot on " + ascent['route_name'], file=sys.stderr )
+                print_err( ascent["climber"] + " is an idiot on " + ascent['route_name'] )
                 fair += 1
         elif asc_grade > grade:
             hard += 1
         else:
-            print( "INCONCEIVABLE!", file=sys.stderr )
+            print_err( "INCONCEIVABLE!" )
             fair += 1
 
     return {
@@ -46,33 +52,71 @@ def softness(ascents, grade):
     }
 
 
+def cragsFromJson(dataPath):
+    cragsList = []
+    with open(dataPath) as f:
+        crags = ijson.items(f, 'crags.item')
+        for crag in crags:
 
-dataPath = 'data/rrg.json'
+            # geocode the crag
+            location = crag['location']
+            geocoded = Geocoder.geocode(location)
+            print( "{} ({}) --> {}".format(crag['name'], crag['location'], geocoded) )
+            crag["coordinates"] = geocoded[0].coordinates
 
-output = []
+            for route in crag['route']:
+                # get softness of route
+                route.update( softness(route['ascents'], route['grade']) )
 
-with open(dataPath) as f:
-    crags = ijson.items(f, 'crags.item')
-    for crag in crags:
+                # remove unneded fields to reduce size of output
+                for field in ['ascents', 'index', 'thumbs_up', 'f_os', 'crag']:
+                    del route[field]
 
-        # geocode the crag
-        location = crag['location']
-        geocoded = Geocoder.geocode(location)
-        print( "{} ({}) --> {}".format(crag['name'], crag['location'], geocoded) )
-        crag["coordinates"] = geocoded[0].coordinates
+                if (route['total'] == 0):
+                    print( "No ascents for {}".format(route['name']) )
+                    # TODO: delete route in that case
+                else:
+                    print( "   {name} ({grade}): {soft} soft, {fair} fair, {hard} hard / {total}".format(**route) )
 
-        for route in crag['route']:
-            # get softness of route
-            route.update( softness(route['ascents'], route['grade']) )
+            cragsList.append(crag)
+    return cragsList
 
-            # remove unneded fields to reduce size of output
-            for field in ['ascents', 'index', 'thumbs_up', 'f_os', 'crag']:
-                del route[field]
+def cragsFromPickle(dataPath):
+    with open(dataPath) as f:
+        unpickler = cPickle.Unpickler(f)
+        cragsList = unpickler.load()
+    return cragsList
+        
 
-            if (route['total'] == 0):
-                print( "No ascents for {}".format(route['name']) )
-                # TODO: delete route in that case
-            else:
-                print( "   {name} ({grade}): {soft} soft, {fair} fair, {hard} hard / {total}".format(**route) )
 
-        output.append(crag)
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print( "Useage: {} <data_file.json | cragList.pickle>" )
+        sys.exit(1)
+
+    dataPath = sys.argv[1]
+    if dataPath.endswith(".pickle"):
+        ## load from pickle
+        crags = cragsFromPickle(dataPath)
+
+        for crag in crags:
+            print( "{name}".format(**crag) )
+            for route in crag['route']:
+                print( "   {name} ({grade}): {soft} soft, {fair} fair, {hard} hard / {total}".format(**route) ) 
+
+    elif dataPath.endswith(".json"):
+        ## load from json
+        crags = cragsFromJson(dataPath)
+
+        # pickle it, now that we've got it
+        pickleName = 'pickles/{}.pickle'.format(os.path.basename(dataPath).split('.')[0])
+        with open(pickleName, 'w') as f:
+            print_err("Pickling to {}...".format(pickleName))
+            pickler = cPickle.Pickler(f)
+            pickler.dump(crags)
+    else:
+        print_err( "Unknown filetype for " + dataPath)
+        sys.exit(1)
+
+
+
