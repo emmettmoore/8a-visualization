5cfcfa112a604b30f7acc3fabee9b48b762d5805
Emmett Moore <ewmoore823@gmail.com>
2014-12-14 16:43:09 -0500

    processed all the places

--- emmett-fiddling/crag-map.js
+++ emmett-fiddling/crag-map.js
@@ -1,41 +1,56 @@
 var crag_map = new Datamap({
     element: document.getElementById('crag-map'),
     scope:   'usa',
-    geographyConfig: {
-        popupOnHover: false,
-        highlightOnHover: false,
-        borderColor: '#FFB680'
-    },
     fills: { 
         'RRG': '#7a8cff',
         'NRG': '#ffe7d8',
-        'RUM': '#ffa89d',
-        defaultFill: 'rgba(255,130,87,0.9)' 
+        'RUM': '#ff3240',//#ffa89d
+        defaultFill: 'rgba(229,255,218,0.9)' //248 255 231
     }, 
     data: {
         'RRG': {fillKey: 'RRG'},
         'NRG': {fillKey: 'NRG'},
         'RUM': {fillKey: 'RUM'},
-   }
+   },
+    geographyConfig: {
+        popupOnHover: false,
+        highlightOnHover: false,
+        borderColor: '#FFB680'
+    },
+    bubblesConfig: {
+        borderWidth: 2,
+        borderColor: '#FFFFFF',
+        popupOnHover: true,
+        popupTemplate: function(geography, data) {
+            return '<div class="hoverinfo"><strong>' + data.name + '</strong></div>';
+        },
+        fillOpacity: 0.75,
+        highlightFillColor: '#FC8D59',
+        highlightBorderColor: 'rgba(250, 15, 160, 0.2)',
+        highlightBorderWidth: 2,
+        highlightFillOpacity: 0.85
+}
 });
 
 
 var crags = [{
           name: 'RRG',
-          radius: 10,
+          radius: 20,
           fillKey: 'RRG',
           latitude: 37.80,
-          longitude: -83.70
+          longitude: -83.70,
+          hoverColor: '#7a8cff',
+          highlightOnHover: false
 },{
           name: 'NRG',
-          radius: 6,
+          radius: 14,
           fillKey: 'NRG',
           latitude: 38.05,
           longitude: -81.11
 
 },{
           name: 'RUM',
-          radius: 4,
+          radius: 17,
           fillKey: 'RUM',
           latitude: 43.80,
           longitude: -71.81
--- process-data.py
+++ process-data.py
@@ -9,13 +9,16 @@ from pygeocoder import Geocoder
 def print_err(msg):
     print(msg, file=sys.stderr)
 
-def softness(ascents, grade):
-    soft = 0
-    fair = 0
-    hard = 0
+def softness(ascents, grade, crag):
+
+    soft     = 0.
+    fair     = 0.
+    hard     = 0.
+    total    = 0.
+    fairness = 0.
     for ascent in ascents:
         if not type(ascent) is dict:
-            print_err( "ERROR: ascent is a {}: {}".format(type(ascent), ascent) )
+            print_err( "ERROR: ascent is a {}: {} in crag {}".format(type(ascent), ascent, crag ))
             continue
 
         asc_grade = ascent['grade']
@@ -25,6 +28,7 @@ def softness(ascents, grade):
             print_err( "Not a real grade: {}".format(asc_grade) )
             continue
 
+        # grades are lexicographically comparable 
         if asc_grade == grade:
             if asc_comment.startswith("Soft"):
                 soft += 1
@@ -33,51 +37,89 @@ def softness(ascents, grade):
             else:
                 fair += 1
         elif asc_grade < grade:
-            if not asc_comment.startswith("Hard"):  # grades are lexicographically comparable, nice
+            if not asc_comment.startswith("Hard"):  
                 soft += 1
             else:
-                print_err( ascent["climber"] + " is an idiot on " + ascent['route_name'] )
+                #print_err( ascent["climber"] + " is an idiot on " + ascent['route_name'] )
                 fair += 1
         elif asc_grade > grade:
             hard += 1
         else:
             print_err( "INCONCEIVABLE!" )
             fair += 1
+        # sum final stats
+        total = soft+fair+hard
+        if (soft - hard) == 0:
+            fairness = 0.0
+        elif (soft > hard):
+            fairness = -1 * soft/total
+        else:
+            fairness = hard / total
+
 
     return {
         "soft": soft,
         "fair": fair,
         "hard": hard,
-        "total": soft+fair+hard
+        "total": total,
+        "fairness": fairness
     }
 
 
 def cragsFromJson(dataPath):
+    FAIR_THRESHOLD = 0.30
     cragsList = []
+    crag_avg_fairness = 0.
+    crag_total_fairness = 0.
+    num_hard = 0
+    num_soft = 0
+    num_fair = 0
     with open(dataPath) as f:
         crags = ijson.items(f, 'crags.item')
         for crag in crags:
 
             # geocode the crag
-            location = crag['location']
-            geocoded = Geocoder.geocode(location)
-            print( "{} ({}) --> {}".format(crag['name'], crag['location'], geocoded) )
-            crag["coordinates"] = geocoded[0].coordinates
-
+            if 'location' in crag:
+                location = crag['location']
+                geocoded = Geocoder.geocode(location)
+                print( "{} ({}) --> {}".format(crag['name'], crag['location'], geocoded) )
+                crag["coordinates"] = geocoded[0].coordinates
+            else:
+                crag['coordinates'] = None
             for route in crag['route']:
                 # get softness of route
-                route.update( softness(route['ascents'], route['grade']) )
-
+                route.update( softness(route['ascents'], route['grade'], crag['name'] ))
+                crag_total_fairness += route['fairness']
+                if route['fairness'] > FAIR_THRESHOLD:
+                    num_hard+= 1;
+                elif route['fairness'] < (-1 * FAIR_THRESHOLD):
+                    num_soft+= 1;
+                else:
+                    num_fair+= 1;
                 # remove unneded fields to reduce size of output
                 for field in ['ascents', 'index', 'thumbs_up', 'f_os', 'crag']:
                     del route[field]
 
                 if (route['total'] == 0):
-                    print( "No ascents for {}".format(route['name']) )
+                    print( "No ascents for {}".format(route['name'].encode('utf-8')) )
                     # TODO: delete route in that case
                 else:
-                    print( "   {name} ({grade}): {soft} soft, {fair} fair, {hard} hard / {total}".format(**route) )
-
+                    pass
+                    #print( "   {name} ({grade}): {soft} soft, {fair} fair, {hard} hard / {total}".format(**route) )
+            crag['route'] = [route for route in crag['route'] if route['total'] > 0]
+            print("crag_total_fairness {}".format(crag_total_fairness))
+            print("num_routes: {}".format(len(crag['route'])))
+            print("num_soft: {}".format(num_soft))
+            print("num_hard: {}".format(num_hard))
+            print("num_fair: {}".format(num_fair))
+            crag_avg_fairness = crag_total_fairness / len(crag['route'])
+            crag.update({"fairness": crag_avg_fairness, 'num_soft': num_soft, 'num_hard': num_hard, 'num_fair': num_fair})
+            print("crag: {name}, fairness: {fairness}".format(**crag))
+            num_soft = 0
+            num_hard = 0
+            num_fair = 0
+            crag_avg_fairness = 0.
+            crag_total_fairness = 0.
             cragsList.append(crag)
     return cragsList
 
@@ -102,7 +144,7 @@ if __name__ == '__main__':
         for crag in crags:
             print( "{name}".format(**crag) )
             for route in crag['route']:
-                print( "   {name} ({grade}): {soft} soft, {fair} fair, {hard} hard / {total}".format(**route) ) 
+                print( "    {name} ({grade}): {soft} soft, {fair} fair, {hard} hard / {total}\n        Fairness: {fairness}\n".format(**route) ) 
 
     elif dataPath.endswith(".json"):
         ## load from json
