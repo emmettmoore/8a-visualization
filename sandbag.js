f5f14e5b8476a96068bf88a5f89a2bb2053a6294
Gabe Joseph <gjoseph92@gmail.com>
2014-12-15 19:01:29 -0500

    Scale graph to container, with 45deg labels

--- sandbag.js
+++ sandbag.js
@@ -36,12 +36,16 @@ function sandBagGraph() {
 	var onHover = function(d, i){};
 	var offHover = function(d, i){};
 	var sb = sandBag();
+	var padding = 0.2;
 
 	function my(selection) {
 		selection.each(function(data) {
 			var maxRows = d3.max(data, function(d) { return d.route.length });
 			height.domain([0, maxRows]);
 
+			var totalBarWidth = 100.0 / data.length,
+				barMargin = totalBarWidth * (padding / 2.0)
+				barWidth = totalBarWidth - (2*barMargin);
 
 			var graphs = d3.select(this).selectAll(".bar").data( data );
 
@@ -61,11 +65,15 @@ function sandBagGraph() {
 			newGraphs
 				.append("div")
 				.classed("bar-label", true)
+				.classed("bar-label-vert", true)
 				;
 
 			graphs.selectAll(".bar-label")
 				.text(function(d) { return d.name; } );
-			// graphs
+			
+			graphs
+				.style("width", barWidth+"%")
+				.style("margin", "0 "+barMargin+"%")
 				// .style('height', function(d) { return height(d.route.length) + "%" });
 
 			graphs.selectAll(".sandbag").data(function(d) { 
